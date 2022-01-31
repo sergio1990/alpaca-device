3be26cdbf0f0158261d47b0bf70d9e3105d4ef9d
Sergio Gernyak <sergeg1990@gmail.com>
2022-01-31 21:41:00 +0200

    Add configuration object

--- lib/alpaca_device.rb
+++ lib/alpaca_device.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "alpaca_device/version"
+require_relative "alpaca_device/configuration"
 
 module AlpacaDevice
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def config
+      @config ||= Configuration.new
+    end
+
+    def configure
+      yield(config)
+    end
+  end
 end
---
+++ lib/alpaca_device/configuration.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module AlpacaDevice
+  class Configuration
+    attr_accessor :discovery_port
+    attr_accessor :alpaca_port
+  end
+end
