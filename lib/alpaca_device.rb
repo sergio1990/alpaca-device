70c4074b0561eb5a3d8573f92e910ae440313404
Sergio Gernyak <sergeg1990@gmail.com>
2022-01-31 22:25:19 +0200

    Add base focuser class autoloading

--- lib/alpaca_device.rb
+++ lib/alpaca_device.rb
@@ -9,6 +9,10 @@ module AlpacaDevice
   autoload :DiscoveryService, 'alpaca_device/discovery_service'
   autoload :Api, 'alpaca_device/api'
 
+  module AscomDevices
+    autoload :BaseFocuser, 'alpaca_device/ascom_devices/base_focuser'
+  end
+
   class << self
     def config
       @config ||= Configuration.new
