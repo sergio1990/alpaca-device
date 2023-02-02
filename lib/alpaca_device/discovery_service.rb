4b15b2f245bb3073d13b9c5ce9f22f7b9dcd044e
Sergey Gernyak <serhii.herniak@thredup.com>
2023-02-02 20:03:36 +0200

    Add logger into discovery service
    Also add checking for the correct discovery message to be received

--- lib/alpaca_device/discovery_service.rb
+++ lib/alpaca_device/discovery_service.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'socket'
+require 'logger'
 
 module AlpacaDevice
   class DiscoveryService
@@ -8,17 +9,27 @@ module AlpacaDevice
       @discovery_host = configuration.discovery_host
       @discovery_port = configuration.discovery_port
       @alpaca_port = configuration.alpaca_port
+
+      @logger = Logger.new(STDOUT)
+      @logger.level = :info
     end
 
     def run
       server = UDPSocket.new
       server.bind(discovery_host, discovery_port)
 
+      logger.info('DiscoveryService') { "Discovery service started at #{discovery_host}:#{discovery_port}..." }
+
       loop do
         text, sender = server.recvfrom(24)
         remote_port = sender[1]
         remote_host = sender[3]
-        server.send "{\"alpacaport\": #{alpaca_port}}", 0, remote_host, remote_port
+
+        logger.info('DiscoveryService') { "Received message '#{text}' from #{remote_host}:#{remote_port}" }
+
+        if text == 'alpacadiscovery1'
+          server.send "{\"alpacaport\": #{alpaca_port}}", 0, remote_host, remote_port
+        end
       end
 
       server.close
@@ -26,6 +37,6 @@ module AlpacaDevice
 
     private
 
-    attr_reader :discovery_host, :discovery_port, :alpaca_port
+    attr_reader :discovery_host, :discovery_port, :alpaca_port, :logger
   end
 end
